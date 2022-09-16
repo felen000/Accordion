6329774ad1c50fa26a4c30b2a3d88be6c0efd360
felen <sapahovarsen@gmail.com>
2022-09-16 20:53:36 +0400

    Добавлен второй аккордеон

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 document.querySelectorAll('.accordion-item__trigger').forEach(item => {
 	item.addEventListener('click', () => {
-		const parent = item.parentElement;
+		const accordionItem = item.parentElement;
+		const accordionName = accordionItem.parentElement.getAttribute('class')
 
-		if (parent.classList.contains('active')) {
-			parent.classList.remove('active')
+		console.log(accordionName);
+
+		if (accordionName == 'accordion-1') {
+			accordionItem.classList.toggle('active')
 		} else {
-			document.querySelectorAll('.accordion-item').forEach(child => child.classList.remove('active'))
-			parent.classList.add('active')
+			if (accordionItem.classList.contains('active')) {
+				accordionItem.classList.remove('active')
+			} else {
+				document.querySelectorAll('.accordion-item').forEach(child => child.classList.remove('active'))
+				accordionItem.classList.add('active')
+			}
 		}
+
+
 	})
 })
